f6f1c79623cf88f651a484574b9850b389d54161
quanli <ailoveliquan@outlook.com>
2020-05-25 14:23:09 +0800

    Create LangParser.java

---
+++ src/main/java/net/leezw/lineparser/parsers/LangParser.java
@@ -0,0 +1,26 @@
+package com.elong.hotel.quan.parsers;
+
+import com.elong.hotel.quan.ObjParser;
+
+import java.io.Serializable;
+
+public class LangParser<T> implements ObjParser<T>,Serializable {
+    private static final long serialVersionUID = 8973668223021498425L;
+
+    private Class clazz;
+
+    public LangParser(Class cl) {
+        clazz = cl;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override public T parser(String value) {
+        T t = null;
+        try {
+            t = (T) clazz.getConstructor(String.class).newInstance(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+}
